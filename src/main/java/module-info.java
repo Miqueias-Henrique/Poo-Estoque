4a7a69c9a38ff46253c65a4968a960d56feedfd8
Maitoguyy <133240713+Maitoguyy@users.noreply.github.com>
2025-11-30 17:56:03 -0300

    comit inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.pooestoque {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.pooestoque to javafx.fxml;
+    exports com.example.pooestoque;
+}
